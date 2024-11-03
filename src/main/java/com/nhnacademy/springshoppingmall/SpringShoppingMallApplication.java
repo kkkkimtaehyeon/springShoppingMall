51ef2188159a1e4af3a2217e4a9da6cd4586c728
kkkkimtaehyeon <kth1927@naver.com>
2024-11-03 23:54:27 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/springshoppingmall/SpringShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShoppingMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/springshoppingmall/SpringShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
